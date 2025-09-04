f651ec43d109e99cf8ed10bee859ba74190d5259
Svetlana Yakutina <svetlana.yakutina@web.de>
2025-09-04 14:08:42 +0200

    Update vis_app.py

--- data/6_Visualisierung/vis_app.py
+++ data/6_Visualisierung/vis_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("seite_1.py", title="Über das Projekt"),
-    st.Page("seite_2.py", title="Visualisierung")
+    st.Page("seite_2.py", title="Netzwerk"),
+    st.Page("seite_3.py", title="Räumliche Visualisierung"),
 ])
 st.set_page_config(page_title="Digitalisat2Graph")
 pg.run()
